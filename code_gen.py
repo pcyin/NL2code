1c873b475ca793ce9554489cc2e9ea52e7ffdc3f
Pengcheng Yin <pcyin1992@gmail.com>
2016-11-11 22:26:48 -0500

    arg parser

--- code_gen.py
+++ code_gen.py
@@ -4,6 +4,7 @@ import ast
 import traceback
 import argparse
 import os
+import logging
 from vprof import profiler
 
 from model import Model
@@ -18,23 +19,30 @@ from tree import Tree
 from nn.utils.generic_utils import init_logging
 from nn.utils.io_utils import deserialize_from_file, serialize_to_file
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    sub_parsers = parser.add_subparsers(dest='operation')
-    train_parser = sub_parsers.add_parser('train')
-    test_parser = sub_parsers.add_parser('decode')
-    interactive_parser = sub_parsers.add_parser('interactive')
-    evaluate_parser = sub_parsers.add_parser('evaluate')
+parser = argparse.ArgumentParser()
+sub_parsers = parser.add_subparsers(dest='operation')
+train_parser = sub_parsers.add_parser('train')
+decode_parser = sub_parsers.add_parser('decode')
+interactive_parser = sub_parsers.add_parser('interactive')
+evaluate_parser = sub_parsers.add_parser('evaluate')
 
+def parse_args():
     parser.add_argument('-data')
     parser.add_argument('-model', default=None)
     parser.add_argument('-conf', default='config.py', help='config file name')
+
+    decode_parser.add_argument('-saveto', default='decode_results.bin')
+    decode_parser.add_argument('-type', default='test_data')
+
+    evaluate_parser.add_argument('-input', default='decode_results.bin')
+    evaluate_parser.add_argument('-type', default='test_data')
+
     args = parser.parse_args()
 
     return args
 
 if __name__ == '__main__':
-    init_logging('parser.log')
+    init_logging('parser.log', logging.INFO)
     args = parse_args()
 
     logging.info('current config: %s', config_info)
@@ -57,20 +65,29 @@ if __name__ == '__main__':
         if args.model:
             model.load(args.model)
 
+    if args.operation == 'train':
+        # train_data_sample = train_data.get_dataset_by_ids(range(10), 'train_sample')
+        # dev_data_sample = dev_data.get_dataset_by_ids(range(10), 'dev_sample')
+        learner = Learner(model, train_data, dev_data)
+        learner.train()
+
     if args.operation == 'decode':
-        dataset = test_data.get_dataset_by_ids([1,2,3,4,5,6,7,8,9,10], name='sample')
+        short_examples = [e for e in test_data.examples if e.parse_tree.size <= 2]
+        for e in short_examples:
+            print e.parse_tree
+        print 'short examples num: ', len(short_examples)
+
+        # dataset = test_data # test_data.get_dataset_by_ids([1,2,3,4,5,6,7,8,9,10], name='sample')
         # cProfile.run('decode_dataset(model, dataset)', sort=2)
-        decode_dataset(model, dataset)
+        dataset = eval(args.type)
 
-
-    # profiler.run(model.decode, 'h', args=(example, train_data.grammar, train_data.terminal_vocab),
-    #              kwargs={'beam_size': 30, 'max_time_step': 15},
-    #              host='localhost', port=8000)
-
+        decode_results = decode_dataset(model, dataset)
+        serialize_to_file(decode_results, args.saveto)
 
     if args.operation == 'evaluate':
-        decode_results = deserialize_from_file('test_data.decode_results.epoch15')
-        dataset = test_data
+        decode_results_file = args.input
+        dataset = eval(args.type)
+        decode_results = deserialize_from_file(decode_results_file)
 
         evaluate_decode_results(dataset, decode_results)
 
@@ -111,4 +128,6 @@ if __name__ == '__main__':
                     print '-' * 60
                 finally:
                     print cand.tree.__repr__()
+                    print 'n_timestep: %d' % cand.n_timestep
+                    print 'ast size: %d' % cand.tree.size
                     print '*' * 60
