36485fa4560635ebe59f8cfcf3645ce34de4cf96
Pengcheng Yin <pcyin1992@gmail.com>
2016-11-23 20:15:59 -0500

    added data iterator

--- learner.py
+++ learner.py
@@ -24,7 +24,7 @@ class Learner(object):
         if val_data:
             logging.info('validation set [%s] (%d examples)', val_data.name, val_data.count)
 
-    def train(self, shuffle=True):
+    def train(self):
         dataset = self.train_data
         nb_train_sample = dataset.count
         index_array = np.arange(nb_train_sample)
@@ -39,11 +39,14 @@ class Learner(object):
         history_valid_perf = []
         best_model_params = None
 
-        for epoch in range(nb_epoch):
-            if shuffle:
-                np.random.shuffle(index_array)
+        train_data_iter = DataIterator(self.train_data, batch_size)
 
-            batches = make_batches(nb_train_sample, batch_size)
+        for epoch in range(nb_epoch):
+            train_data_iter.reset()
+            # if shuffle:
+            #     np.random.shuffle(index_array)
+
+            # batches = make_batches(nb_train_sample, batch_size)
 
             # epoch begin
             sys.stdout.write('Epoch %d' % epoch)
@@ -51,11 +54,12 @@ class Learner(object):
             cum_nb_examples = 0
             loss = 0.0
 
-            for batch_index, (batch_start, batch_end) in enumerate(batches):
+            # for batch_index, (batch_start, batch_end) in enumerate(batches):
+            for batch_index, (examples, batch_ids) in enumerate(train_data_iter):
                 cum_updates += 1
 
-                batch_ids = index_array[batch_start:batch_end]
-                examples = dataset.get_examples(batch_ids)
+                # batch_ids = index_array[batch_start:batch_end]
+                # examples = dataset.get_examples(batch_ids)
                 cur_batch_size = len(examples)
 
                 inputs = dataset.get_prob_func_inputs(batch_ids)
@@ -110,3 +114,46 @@ class Learner(object):
 
         logging.info('training finished, save the best model')
         np.savez('model.npz', **best_model_params)
+
+
+class DataIterator:
+    def __init__(self, dataset, batch_size=10):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.index_array = np.arange(self.dataset.count)
+        self.ptr = 0
+        self.buffer_size = batch_size * 5
+        self.buffer = []
+
+    def reset(self):
+        self.ptr = 0
+        self.buffer = []
+        np.random.shuffle(self.index_array)
+
+    def __iter__(self):
+        return self
+
+    def next_batch(self):
+        batch = self.buffer[:self.batch_size]
+        del self.buffer[:self.batch_size]
+
+        batch_ids = [e.eid for e in batch]
+
+        return batch, batch_ids
+
+    def next(self):
+        if self.buffer:
+            return self.next_batch()
+        else:
+            if self.ptr >= self.dataset.count:
+                raise StopIteration
+
+            self.buffer = self.index_array[self.ptr:self.ptr + self.buffer_size]
+
+            # sort buffer contents
+            examples = self.dataset.get_examples(self.buffer)
+            self.buffer = sorted(examples, key=lambda e: len(e.actions))
+
+            self.ptr += self.buffer_size
+
+            return self.next_batch()
